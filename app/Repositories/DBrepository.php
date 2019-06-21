41b3d2922bcd51001753218993cb6b295a4f82e4
Bishal Sahoo <mavericksthinker@gmail.com>
2019-06-21 10:55:49 +0700

    Included why we use abstract transformers instead of traits

--- app/Repositories/DBrepository.php
+++ app/Repositories/DBrepository.php
@@ -6,6 +6,13 @@ namespace App\Repositories;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Creation of an abstract repository class instead of trait is that the method 
+ * let's say here getAll() can be overridden and modified the way required as there might be a possibility 
+ * that the data from getAll() might be required in a different orientation
+ * Class DBRepository
+ * @package App\Repositories
+ */
 abstract class DBRepository
 {
 
